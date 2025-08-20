83ca07ab6e80bd5a8e10a371036377abaec22b4a
m5tibi <vargat77@gmail.com>
2025-08-20 21:47:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# bot.py (V15.3 - Végleges Aszinkron Javítással)
+# bot.py (V15.4 - Végleges Aszinkron Javítással)
 
 import os
 import telegram
@@ -12,7 +12,7 @@ import secrets
 from functools import wraps
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, filters, ConversationHandler
-from supabase_async import create_client as create_async_client, AsyncClient
+from supabase_py_async import create_client as create_async_client, AsyncClient
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
@@ -28,6 +28,8 @@ ADMIN_CHAT_ID = 1326707238
 
 # --- Konverziós Állapotok ---
 AWAITING_CODE, AWAITING_BROADCAST, AWAITING_CODE_COUNT = range(3)
+
+# ... (a fájl többi része változatlan)
 
 # --- Dekorátorok ---
 def admin_only(func):
@@ -374,3 +376,4 @@ def add_handlers(application: Application):
     
     print("Minden parancs- és gombkezelő sikeresen hozzáadva.")
     return application
+
