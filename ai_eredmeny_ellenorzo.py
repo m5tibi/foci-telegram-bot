ddbd502148a65d9ac9db397fb687a7e99953e9b6
m5tibi <vargat77@gmail.com>
2026-08-11 11:09:33 +0200

    Update ai_eredmeny_ellenorzo.py

--- ai_eredmeny_ellenorzo.py
+++ ai_eredmeny_ellenorzo.py
@@ -35,9 +35,9 @@ SPORT_KEYS = [
     "soccer_brazil_campeonato", "soccer_argentina_primera_division",
     "soccer_usa_mls", "soccer_mexico_ligamx",
     "soccer_japan_j_league", "soccer_australia_aleague",
-    "soccer_uefa_champs_league_qualifying",
-    "soccer_uefa_europa_league_qualifying",
-    "soccer_uefa_europa_conference_league_qualifying",
+    "soccer_uefa_champs_league_qualification",
+    "soccer_korea_kleague1", "soccer_saudi_arabia_pro_league",
+    "soccer_chile_campeonato",
 ]
 
 
